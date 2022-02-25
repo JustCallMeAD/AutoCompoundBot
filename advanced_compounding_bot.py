4f8696c42c6da9e82b5c50a91ff6cbefa164dc22
JannikMottulla <38241107+JannikMottulla@users.noreply.github.com>
2022-02-25 19:33:37 +0100

    Update advanced_compounding_bot.py

--- advanced_compounding_bot.py
+++ advanced_compounding_bot.py
@@ -8,42 +8,6 @@ class AdvancedCompoundingBot:
         self.timers = {
             "refresh_interval": ""
         }
-
-    def refresh_website(self):
-        chrome_tab_location = pyautogui.locateOnScreen('chrome-tab.png', confidence=0.95)
-        if chrome_tab_location == None:
-
-            print("please open a browser with the theanimal.farm website")
-        else:
-            chrome_tab_location_center = pyautogui.center(chrome_tab_location)
-            pyautogui.click(chrome_tab_location_center.x, chrome_tab_location_center.y)
-            pyautogui.press('f5')
-            time.sleep(4)
-
-    def goto_garden_tab(self):
-        garden_tab_location = pyautogui.locateOnScreen('drip-garden-tab.png', confidence=0.95)
-        if garden_tab_location == None:
-            print("could not find the drip garden tab")
-        else:
-            garden_tab_location_center = pyautogui.center(garden_tab_location)
-            time.sleep(1)
-            pyautogui.click(garden_tab_location_center.x, garden_tab_location_center.y)
-
-    def adjust_position(self):
-        time.sleep(1)
-        garden_heading_location = pyautogui.locateOnScreen('garden-heading.png', confidence=0.95)
-        plant_status_found = False
-        compound_button_found = False
-        if garden_heading_location != None:
-            garden_heading_location_center = pyautogui.center(garden_heading_location)
-            pyautogui.click(garden_heading_location_center.x, garden_heading_location_center.y)
-        while(plant_status_found != True or compound_button_found != True):
-            plant_status_found = pyautogui.locateOnScreen('plant-status.png', confidence=0.80) != None
-            compound_button_found = pyautogui.locateOnScreen('compound-button.png', confidence=0.95) != None or pyautogui.locateOnScreen('compound-button-not-ready.png', confidence=0.95) != None
-            if plant_status_found and compound_button_found:
-                break
-            pyautogui.scroll(-80)
-
     def compound(self):
         time.sleep(1)
         compound_button_location = pyautogui.locateOnScreen('compound-button.png', confidence=0.95)
@@ -55,24 +19,25 @@ class AdvancedCompoundingBot:
             mm_location = pyautogui.locateOnScreen('mm.png', confidence=0.50)
             if mm_location != None:
                 mm_center = pyautogui.center(mm_location)
+                print("mm position: ", mm_center.x, mm_center.y)
                 pyautogui.click(mm_center.x, mm_center.y)
                 time.sleep(1)
                 pyautogui.scroll(-80)
                 time.sleep(1)
                 pyautogui.click(1820, 610)
                 time.sleep(10)
-                return self.check_if_compound_succes()
+                compound_success = self.check_if_compound_succes() 
+                if compound_success == True:
+                    return True
             else:
-                return True
+                return False
+        print('compound button not found or not ready')
         return True
 
     def check_if_compound_succes(self):
         return pyautogui.locateOnScreen('mm.png', confidence=0.85) == None and pyautogui.locateOnScreen('ready.png', confidence=0.95) != None
 
     def cycle(self):
-        self.refresh_website()
-        self.goto_garden_tab()
-        self.adjust_position()
         while True:
             if not self.compound():
                 print("breaking")
@@ -82,4 +47,4 @@ class AdvancedCompoundingBot:
 
 
 x = AdvancedCompoundingBot()
-x.cycle() 
+x.cycle()
